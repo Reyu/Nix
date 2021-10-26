766f0edcc8d19bdaba773d696561f4803f431117
Reyu Zenfold <reyu@reyuzenfold.com>
2021-10-26 10:46:40 -0400

    Remove NFS mounts until KRB is setup

--- hosts/loki/filesystems.nix
+++ hosts/loki/filesystems.nix
@@ -9,25 +9,25 @@
       device = "/dev/disk/by-uuid/FA23-833F";
       fsType = "vfat";
     };
-    "/media/ISO" = {
-      device = "burrow:/data/media/ISO";
-      fsType = "nfs";
-      options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
-    };
-    "/media/Music" = {
-      device = "burrow:/data/media/audio/music";
-      fsType = "nfs";
-      options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
-    };
-    "/media/Movies" = {
-      device = "burrow:/data/media/video/movies";
-      fsType = "nfs";
-      options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
-    };
-    "/media/Television" = {
-      device = "burrow:/data/media/video/television";
-      fsType = "nfs";
-      options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
-    };
+    # "/media/ISO" = {
+    #   device = "burrow:/data/media/ISO";
+    #   fsType = "nfs";
+    #   options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
+    # };
+    # "/media/Music" = {
+    #   device = "burrow:/data/media/audio/music";
+    #   fsType = "nfs";
+    #   options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
+    # };
+    # "/media/Movies" = {
+    #   device = "burrow:/data/media/video/movies";
+    #   fsType = "nfs";
+    #   options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
+    # };
+    # "/media/Television" = {
+    #   device = "burrow:/data/media/video/television";
+    #   fsType = "nfs";
+    #   options = [ "x-systemd.automount" "x-systemd.idle-timeout=600" "noauto" ];
+    # };
   };
 }
