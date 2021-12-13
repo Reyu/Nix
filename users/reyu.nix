a47dabb9bd878d0a8b2643856ae297563c4505a4
reyu <reyu@reyuzenfold.com>
2021-12-12 20:49:58 -0500

    Add SSH Pub key

--- users/reyu.nix
+++ users/reyu.nix
@@ -6,7 +6,11 @@
     description = "Reyu Zenfold";
     extraGroups = [ "wheel" "audio" ];
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPPxK6wj41rJ00x3SSA8qw/c7WjmUW4Z1xshAQxAciS8 reyu@kp"
+    ];
   };
 
+  # Allow to run nix
   nix.allowedUsers = [ "reyu" ];
 }
--- users/root.nix
+++ users/root.nix
@@ -1,11 +1,11 @@
 { config, pkgs, lib, ... }: {
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users = {
-
-    users.root = {
-      isNormalUser = true;
-    };
+  users.users.root = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPPxK6wj41rJ00x3SSA8qw/c7WjmUW4Z1xshAQxAciS8 reyu@kp"
+    ];
   };
 
   # Allow to run nix
