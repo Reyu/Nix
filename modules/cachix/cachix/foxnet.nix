40731e5fa1400df371d2a4a3378144b5a2a8b196
reyu <reyu@reyuzenfold.com>
2021-12-16 16:37:59 -0500

    Update binary caches

---
+++ modules/cachix/cachix/foxnet.nix
@@ -0,0 +1,11 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://foxnet.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "foxnet.cachix.org-1:YPicMNP3X2sGUHpcT2bjJCnCVY1OL2VwgEQyFa8a7A8="
+    ];
+  };
+}
