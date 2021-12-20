cfd5f1fc6ad7fefad1691d6ef96becebcfc283f7
reyu <reyu@reyuzenfold.com>
2021-12-20 12:17:46 -0500

    Git Ignore tweaks

--- home-manager/modules/git/default.nix
+++ home-manager/modules/git/default.nix
@@ -3,20 +3,19 @@
     git = {
       enable = true;
 
-      ignores = [ "_darcs" "Session.vim" "*~" ".env" ".env.local" ];
-
-      extraConfig = {
-        init.defaultBranch = "main";
-        pull.rebase = false;
-      };
+      userName = "reyu";
+      userEmail = "reyu@reyuzenfold.com";
 
       signing = {
         key = "0x8DA67C907DD6F454";
         signByDefault = true;
       };
 
-      userName = "reyu";
-      userEmail = "reyu@reyuzenfold.com";
+      ignores = [ "_darcs" "Session.vim" "*~" ".env" ".env.local" ".direnv" ];
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = false;
+      };
     };
   };
 }
