063255377bee378cf26a34680e5e5fdb40e58ab0
reyu <reyu@reyuzenfold.com>
2022-01-21 18:23:41 -0500

    Update `htoprc` configs

--- home-manager/modules/shell/default.nix
+++ home-manager/modules/shell/default.nix
@@ -33,8 +33,22 @@
 
   programs.htop = {
     enable = true;
-    settings.tree_view = true;
-    settings.vimMode = true;
+    settings = {
+      account_guest_in_cpu_meter = "1";
+      all_branches_collapsed = "1";
+      color_scheme = "6";
+      column_meter_modes_0 = "1 1 1 1 2 2 2";
+      column_meter_modes_1 = "1 2 2 2 2 2 2";
+      column_meters_0 = "LeftCPUs4 CPU Memory ZFSARC DiskIO NetworkIO Systemd";
+      column_meters_1 = "RightCPUs4 CPU Memory ZFSARC Tasks LoadAverage Uptime";
+      detailed_cpu_time = "1";
+      header_margin = "0";
+      hide_function_bar = "1";
+      highlight_base_name = "1";
+      shadow_other_users = "1";
+      show_program_path = "1";
+      show_thread_names = "1";
+    };
   };
 
   programs.jq.enable = true;
