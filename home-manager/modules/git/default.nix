60bc7b65b7da6721a4237f94d53203fadb065449
reyu <reyu@reyuzenfold.com>
2022-03-22 17:13:20 -0400

    Update Git Ignore filter
    Adds a bunch of global ignores. Includes Linux, Vim, and some shell
    stuff.

--- home-manager/modules/git/default.nix
+++ home-manager/modules/git/default.nix
@@ -15,7 +15,49 @@
         last = "cat-file commit HEAD";
       };
 
-      ignores = [ "_darcs" "Session.vim" "*~" ".env" ".env.local" ".direnv" ];
+      ignores = [
+        ### Nix
+        # Ignore build outputs from performing a nix-build or `nix build` command
+        "result"
+        "result-*"
+        ### GPG
+        "secring.*"
+        ### Linux
+        "*~"
+        # temporary files which can be created if a process still has a handle open of a deleted file
+        ".fuse_hidden*"
+        # KDE directory preferences
+        ".directory"
+        # Linux trash folder which might appear on any partition or disk
+        ".Trash-*"
+        # .nfs files are created when an open file is removed but is still being accessed
+        ".nfs*"
+        ### Vim
+        # Swap
+        "[._]*.s[a-v][a-z]"
+        "[._]*.sw[a-p]"
+        "[._]s[a-rt-v][a-z]"
+        "[._]ss[a-gi-z]"
+        "[._]sw[a-p]"
+        # Session
+        "Session.vim"
+        "Sessionx.vim"
+        # Temporary
+        ".netrwhist"
+        "*~"
+        # Auto-generated tag files
+        "tags"
+        # Persistent undo
+        "[._]*.un~"
+        ### Darcs VCS
+        "_darcs"
+        ### Ansible
+        "*.retry"
+        ### Shell / Other
+        ".env"
+        ".env.local"
+        ".direnv"
+      ];
       extraConfig = {
         init.defaultBranch = "main";
         pull.rebase = false;
