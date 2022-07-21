3d49be51951724b01b8986cc03856d318474d8fc
reyu <reyu@reyuzenfold.com>
2022-07-21 14:28:45 -0400

    Burrow: Update process groups for MAS

--- hosts/burrow/MAS.nix
+++ hosts/burrow/MAS.nix
@@ -2,14 +2,20 @@
   services = {
     plex.enable = true;
     plex.openFirewall = true;
+    plex.group = "media";
     tautulli.enable = true;
+    tautulli.group = "media";
     sonarr.enable = true;
     sonarr.openFirewall = true;
+    sonarr.group = "media";
     radarr.enable = true;
     radarr.openFirewall = true;
+    radarr.group = "media";
     bazarr.enable = true;
     bazarr.openFirewall = true;
+    bazarr.group = "media";
     lidarr.enable = true;
     lidarr.openFirewall = true;
+    lidarr.group = "media";
   };
 }
