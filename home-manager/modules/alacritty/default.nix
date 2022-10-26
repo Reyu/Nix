57396751115e4aa695c5df13ebbbdf80c4ef64e9
reyu <reyu@reyuzenfold.com>
2022-10-26 12:50:01 -0400

    Alacritty: update colors

--- home-manager/modules/alacritty/default.nix
+++ home-manager/modules/alacritty/default.nix
@@ -15,32 +15,32 @@ in
       font.size = 12;
       colors = {
         primary = {
-          background = "#002b36";
-          foreground = "#839496";
+          background = "0x001217";
+          foreground = "0x708183";
         };
         cursor = {
           text = "#002b36";
           cursor = "#839496";
         };
         normal = {
-          black = "#073642";
-          red = "#dc322f";
-          green = "#859900";
-          yellow = "#b58900";
-          blue = "#268bd2";
-          magenta = "#d33682";
-          cyan = "#2aa198";
-          white = "#eee8d5";
+          black = "0x002b36";
+          red = "0xdc322f";
+          green = "0x859900";
+          yellow = "0xb58900";
+          blue = "0x268bd2";
+          magenta = "0xd33682";
+          cyan = "0x2aa198";
+          white = "0xeee8d5";
         };
         bright = {
-          black = "#002b36";
-          red = "#cb4b16";
-          green = "#586e75";
-          yellow = "#657b83";
-          blue = "#839496";
-          magenta = "#6c71c4";
-          cyan = "#93a1a1";
-          white = "#fdf6e3";
+          black = "0x002b36";
+          red = "0xcb4b16";
+          green = "0x586e75";
+          yellow = "0x657b83";
+          blue = "0x839496";
+          magenta = "0x6c71c4";
+          cyan = "0x93a1a1";
+          white = "0xfdf6e3";
         };
       };
     };
