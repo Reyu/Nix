0689993449ba14feee29c6fc32cca1aedddbff89
reyu <reyu@reyuzenfold.com>
2022-12-12 14:29:34 -0500

    Remap window-movement keys

--- home-manager/modules/vim/pack/start/reyu/lua/reyu/plugins/smart-splits.lua
+++ home-manager/modules/vim/pack/start/reyu/lua/reyu/plugins/smart-splits.lua
@@ -8,6 +8,17 @@ smart_splits.setup({
     },
     ignored_buftypes = { },
     default_amount = 3,
-    wrap_at_edge = true,
+    wrap_at_edge = false,
     tmux_integration = true,
 })
+
+vim.keymap.set('n', '<S-A-h>', require('smart-splits').resize_left)
+vim.keymap.set('n', '<S-A-j>', require('smart-splits').resize_down)
+vim.keymap.set('n', '<S-A-k>', require('smart-splits').resize_up)
+vim.keymap.set('n', '<S-A-l>', require('smart-splits').resize_right)
+
+-- moving between splits
+vim.keymap.set('n', '<M-h>', require('smart-splits').move_cursor_left)
+vim.keymap.set('n', '<M-j>', require('smart-splits').move_cursor_down)
+vim.keymap.set('n', '<M-k>', require('smart-splits').move_cursor_up)
+vim.keymap.set('n', '<M-l>', require('smart-splits').move_cursor_right)
