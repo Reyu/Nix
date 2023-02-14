e7e8c4c37027f1cc4c3af1947e77bacd3be75e33
reyu <reyu@reyuzenfold.com>
2023-02-14 12:46:08 -0500

    chg: dont use fuse_allow_other

--- home-manager/modules/impermanence/default.nix
+++ home-manager/modules/impermanence/default.nix
@@ -1,6 +1,6 @@
 { config, ... }: {
   home.persistence."/persist/home/${config.home.username}" = {
-    allowOther = true;
+    allowOther = false;
     directories = [
       ".cache/nvim"
       ".gnupg"
