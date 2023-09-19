171b5c822a4743a97c7a352c72e15a89a5a3134b
reyu <reyu@reyuzenfold.com>
2023-09-18 22:08:12 -0400

    set grub device

--- hosts/hetzner/configuration.nix
+++ hosts/hetzner/configuration.nix
@@ -4,7 +4,7 @@
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
   boot.loader.timeout = 0;
-  boot.loader.grub.device = "nodev";
+  boot.loader.grub.device = "/dev/sda";
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     zfs rollback -r rpool/local/root@blank
   '';
