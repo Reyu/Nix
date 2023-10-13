632e71a5e322fe91fa034efab38391dd4a8c7d3c
reyu <reyu@reyuzenfold.com>
2023-10-13 18:11:11 -0400

    fix(neovim): drop lsp-zero for plain lspconfig

--- home-manager/modules/vim/plugins/lsp.nix
+++ home-manager/modules/vim/plugins/lsp.nix
@@ -1,74 +1,97 @@
 { pkgs, ... }:
 with pkgs.vimPlugins; [
-  nvim-lspconfig
   {
-    plugin = lsp-zero-nvim;
+    plugin = nvim-lspconfig;
     type = "lua";
     config = ''
-        local function map(mode, lhs, rhs, extra_opts, bufnr)
-            local opts = vim.tbl_extend('keep', extra_opts or {},
-                                        {buffer = bufnr, noremap = true, silent = true})
-            vim.keymap.set(mode, lhs, rhs, opts)
-        end
+      local lspconfig = require('lspconfig')
 
-        local function previewLocationCallback(_, result)
-            if result == nil or vim.tbl_isempty(result) then return nil end
-            vim.lsp.util.preview_location(result[1], {border = 'rounded'})
-        end
+      local function previewLocationCallback(_, result)
+        if result == nil or vim.tbl_isempty(result) then return nil end
+        vim.lsp.util.preview_location(result[1], {border = 'rounded'})
+      end
 
-        local function peekDefinition(bufnr)
-            local params = vim.lsp.util.make_position_params(0, 'utf-16')
-            return vim.lsp.buf_request(bufnr, 'textDocument/definition', params,
-                                       previewLocationCallback)
-        end
+      local function peekDefinition(bufnr)
+        local params = vim.lsp.util.make_position_params(0, 'utf-16')
+        return vim.lsp.buf_request(bufnr, 'textDocument/definition', params,
+            previewLocationCallback)
+      end
 
-        local lsp = require('lsp-zero').preset({
-            name = 'system-lsp',
-            manage_nvim_cmp = false
-        })
+      local function genOpts(desc, bufnr)
+        return {
+          desc = desc,
+          buffer = bufnr,
+          noremap = true,
+          silent = true
+        }
+      end
 
-        lsp.on_attach(function(_, bufnr)
-            lsp.default_keymaps({buffer = bufnr, omit = {'K'}})
-
-            map({'n', 'x'}, 'gq',
-                function() vim.lsp.buf.format({async = false, timeout_ms = 10000}) end,
-                {desc = 'Format Buffer/Selection'}, bufnr)
-            map('n', 'K', function()
-                local winid = require('ufo').peekFoldedLinesUnderCursor()
-                if not winid then vim.lsp.buf.hover() end
-            end, {desc = 'Show hover menu'}, bufnr)
-            map('n', '<LocalLeader>p', peekDefinition, {desc = 'Peek definition'}, bufnr)
-            map('n', '<LocalLeader>h',
-                function() require('haskell-tools').hoogle.hoogle_signature() end,
-                {desc = 'Hoogle Signature'}, bufnr)
-            map('n', '<LocalLeader>ca', vim.lsp.buf.code_action,
-                {desc = 'Run code actions'}, bufnr)
-            map('n', '<LocalLeader>cl', vim.lsp.codelens.run, {desc = 'Run Codelens'},
-                bufnr)
-        end)
-
-        lsp.set_server_config({
-            capabilities = {
-                textDocument = {
-                    foldingRange = {dynamicRegistration = false, lineFoldingOnly = true}
-                }
-            }
-        })
-
-        lsp.setup_servers({'bashls', 'dockerls', 'rnix', 'terraformls', force = true})
-        lsp.configure('jsonls', {
-            -- lazy-load schemastore when needed
-            on_new_config = function(new_config)
-                new_config.settings.json.schemas =
-                    new_config.settings.json.schemas or {}
-                vim.list_extend(new_config.settings.json.schemas,
-                                require("schemastore").json.schemas())
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        desc = 'LSP Keybinds',
+        callback = function(event)
+          vim.keymap.set('n', 'K', function()
+              local winid = require('ufo').peekFoldedLinesUnderCursor()
+              if not winid then vim.lsp.buf.hover() end
             end,
-            settings = {json = {format = {enable = true}, validate = {enable = true}}}
-        })
-        lsp.configure('yamlls', {settings = {yaml = {keyOrdering = false}}})
+            genOpts('Show hover menu', event.buf)
+          )
+          vim.keymap.set('n', '<LocalLeader>p', peekDefinition, genOpts('Peek definition', event.buf))
+          vim.keymap.set('n', '<LocalLeader>h', function() require('haskell-tools').hoogle.hoogle_signature() end, genOpts('Hoogle Signature', event.buf))
+          vim.keymap.set('n', '<LocalLeader>ca', vim.lsp.buf.code_action, genOpts('Run code actions', event.buf))
+          vim.keymap.set('n', '<LocalLeader>cl', vim.lsp.codelens.run, genOpts('Run Codelens', event.buf))
+          vim.keymap.set({'n', 'x'}, '<LocalLeader>f', function() vim.lsp.buf.format({async = true}) end, genOpts('Format', event.buf))
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, genOpts('Go to definition', event.buf))
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, genOpts('Go to declaration', event.buf))
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, genOpts('Go to implementation', event.buf))
+          vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, genOpts('Go to type definition', event.buf))
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, genOpts('Go to references', event.buf))
+          vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, genOpts('Signature help', event.buf))
+          vim.keymap.set('n', 'gl', vim.diagnostic.open_float, genOpts('Open diagnostic float', event.buf))
+          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, genOpts('Go to previous diagnostic', event.buf))
+          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, genOpts('Go to next diagnostic', event.buf))
+          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, genOpts('Add workspace folder', event.buf))
+          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, genOpts('Remove workspace folder', event.buf))
+          vim.keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_fjlders())) end,
+	    genOpts('List workspace folders', event.buf))
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, genOpts('Rename', event.buf))
+        end
+      })
 
-        lsp.setup()
+      -- lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+      --   capabilities = {
+      --     textDocument = {
+      --       foldingRange = {dynamicRegistration = false, lineFoldingOnly = true}
+      --     }
+      --   }
+      -- })
+      lspconfig.util.default_config.capabilities = vim.tbl_deep_extend(
+        'force',
+        lspconfig.util.default_config.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
+
+      lspconfig.bashls.setup({})
+      lspconfig.dockerls.setup({})
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {runtime = 'LuaJIT'},
+          diagnostic = { globals = {'vim'}},
+          workspace = {library = {vim.env.VIMRUNTIME}}
+        }
+      })
+      lspconfig.rnix.setup({})
+      lspconfig.jsonls.setup({
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+              new_config.settings.json.schemas =
+                  new_config.settings.json.schemas or {}
+              vim.list_extend(new_config.settings.json.schemas,
+                              require("schemastore").json.schemas())
+          end,
+          settings = {json = {format = {enable = true}, validate = {enable = true}}}
+      })
+      lspconfig.yamlls.setup({settings = {yaml = {keyOrdering = false}}})
     '';
   }
   {
@@ -85,7 +108,6 @@ with pkgs.vimPlugins; [
     type = "lua";
     config = ''
         local haskell_tools = require('haskell-tools')
-        local hls_lsp = require('lsp-zero').build_options('hls', {})
 
         local cmd = 'haskell-language-server-wrapper'
         if vim.fn.executable('haskell-language-server') == 1 then
@@ -99,7 +121,6 @@ with pkgs.vimPlugins; [
             callback = function(ev)
                 haskell_tools.start_or_attach({
             hls = {
-                capabilities = hls_lsp.capabilities,
                 cmd = {cmd, '--lsp'},
                 on_attach = function(_, bufnr)
                     haskell_tools.dap.discover_configurations(ev.buf)
