e7fba5d3de80218cbef862ea3cd5efa8dfe572b8
reyu <reyu@reyuzenfold.com>
2023-11-27 16:56:28 -0500

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'devshell':
        'github:numtide/devshell/1aed986e3c81a4f6698e85a7452cbfcc4b31a36e' (2023-10-27)
      → 'github:numtide/devshell/7ad1c417c87e98e56dcef7ecd0e0a2f2e5669d51' (2023-11-24)
    • Updated input 'github-nvim-theme':
        'github:projekt0n/github-nvim-theme/48f95f763e1a98e508260a20e448c3ff2d91213a' (2023-09-23)
      → 'github:projekt0n/github-nvim-theme/22aa0678517d8f7e5fc84e89162c553e9b281e5b' (2023-11-26)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/993fb02d20760067b8ee19c713d94cee07037759' (2023-11-19)
      → 'github:nix-community/home-manager/db1878f013b52ba5e4034db7c1b63e8d04173a86' (2023-11-27)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/2b00bc76dc893cd996a3d76a2f059d657a5ef37a' (2023-11-19)
      → 'github:NixOS/nixos-hardware/b006ec52fce23b1d57f6ab4a42d7400732e9a0a2' (2023-11-26)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/71d28bf1ae55faeb9f0332767b53ae7d2765d808' (2023-11-20)
      → 'github:nixos/nixpkgs/4716d207e946072d77a9c54fdfa4df238da14476' (2023-11-27)
    • Updated input 'nur':
        'github:nix-community/NUR/4486267d862ccc8fbbac6c112ccf1f0595cfbd74' (2023-11-20)
      → 'github:nix-community/NUR/2aac3f35b161d93b786369d97f1eb956a0f6be11' (2023-11-27)
    • Updated input 'unstable':
        'github:nixos/nixpkgs/e4ad989506ec7d71f7302cc3067abd82730a4beb' (2023-11-19)
      → 'github:nixos/nixpkgs/5a09cb4b393d58f9ed0d9ca1555016a8543c2ac8' (2023-11-24)

--- hosts/loki/configuration.nix
+++ hosts/loki/configuration.nix
@@ -187,7 +187,7 @@ in {
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    configPackages = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
   virtualisation.podman.zfs = true;
