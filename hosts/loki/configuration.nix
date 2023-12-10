98b551b8cad9c008f63125b271ee8d2f4cb2bf13
reyu <reyu@reyuzenfold.com>
2023-12-09 22:42:53 -0500

    Loki: readd streamdeckui

--- hosts/loki/configuration.nix
+++ hosts/loki/configuration.nix
@@ -44,7 +44,7 @@ in {
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
-  # programs.streamdeck-ui.enable = true;
+  programs.streamdeck-ui.enable = true;
 
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.checkReversePath = "loose";
