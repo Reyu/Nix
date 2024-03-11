c18aabdbd61bd3dae3b0657802f528d38d552521
reyu <reyu@reyuzenfold.com>
2024-03-11 11:53:16 -0400

    add nginx for `/jellyseerr` proxy

--- hosts/burrow/MAS.nix
+++ hosts/burrow/MAS.nix
@@ -75,6 +75,48 @@
       group = "media";
       openFirewall = true;
     };
+    nginx = {
+      enable = true;
+      virtualHosts."jellyseerr" = {
+        default = true;
+        locations."^~ /jellyseerr" = {
+          extraConfig = ''
+            set $app 'jellyseerr';
+
+            # Remove /jellyseer path to pass to the app
+            rewrite ^/jellyseerr/?(.*)$ /$1 break;
+            proxy_pass http://127.0.0.1:5055; # NO TRAILING SLASH
+
+            # Redirect location headers
+            proxy_redirect ^ /$app;
+            proxy_redirect /setup /$app/setup;
+            proxy_redirect /login /$app/login;
+
+            # Sub filters to replace hardcoded paths
+            proxy_set_header Accept-Encoding "";
+            sub_filter_once off;
+            sub_filter_types *;
+
+            #https://stackoverflow.com/questions/19700871/how-to-inject-custom-content-via-nginx
+            sub_filter '</head>' '<script language="javascript">(()=>{var t="$app";let e=history.pushState;history.pushState=function a(){arguments[2]&&!arguments[2].startsWith("/"+t)&&(arguments[2]="/"+t+arguments[2]);let s=e.apply(this,arguments);return window.dispatchEvent(new Event("pushstate")),s};let a=history.replaceState;history.replaceState=function e(){arguments[2]&&!arguments[2].startsWith("/"+t)&&(arguments[2]="/"+t+arguments[2]);let s=a.apply(this,arguments);return window.dispatchEvent(new Event("replacestate")),s},window.addEventListener("popstate",()=>{console.log("popstate")})})();</script></head>';
+
+            sub_filter 'href="/"' 'href="/$app"';
+            sub_filter 'href="/login"' 'href="/$app/login"';
+            sub_filter 'href:"/"' 'href:"/$app"';
+            sub_filter '/_next' '/$app\/_next';
+            sub_filter '/_next' '/$app/_next';
+            sub_filter '/api/v1' '/$app/api/v1';
+            sub_filter '/login/plex/loading' '/$app/login/plex/loading';
+            sub_filter '/images/' '/$app/images/';
+            sub_filter '/android-' '/$app/android-';
+            sub_filter '/apple-' '/$app/apple-';
+            sub_filter '/favicon' '/$app/favicon';
+            sub_filter '/logo_' '/$app/logo_';
+            sub_filter '/site.webmanifest' '/$app/site.webmanifest';
+          '';
+        };
+      };
+    };
   };
   virtualisation.oci-containers = {
     backend = "podman";
